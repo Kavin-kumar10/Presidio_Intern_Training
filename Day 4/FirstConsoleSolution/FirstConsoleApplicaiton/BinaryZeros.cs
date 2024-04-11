3bd7d0c7f0ae457aa886a824eceabef74b2e3e26
Kavin-kumar10 <kavinkumar.prof@gmail.com>
2024-04-11 17:05:23 +0530

    Codility first problem included in day 4

--- Day 4/FirstConsoleSolution/FirstConsoleApplicaiton/BinaryZeros.cs
+++ Day 4/FirstConsoleSolution/FirstConsoleApplicaiton/BinaryZeros.cs
@@ -9,21 +9,35 @@ namespace FirstConsoleApplicaiton
     internal class BinaryZeros
     {
         //Ongoing
-        //static int maxConsecutiveZeros(string str)
-        //{
-
-        //    for(int ind = 0;ind < str.Length;ind++)
-        //    {
-        //        if (str[ind] == '1') { }
-        //    }
-        //}
+        static int maxConsecutiveZeros(string str)
+        {
+            int ind = 0;
+            int maxCount = 0;
+            int count = 0;
+            while (str[ind] != '1') ind++;
+            for (; ind < str.Length; ind++)
+            {
+                if (str[ind] == '1') {
+                    if(count>maxCount)maxCount = count;
+                    count = 0;
+                }
+                else
+                {
+                    count++;
+                }
+            }
+            return maxCount;
+        }
         static void Main(string[] args)
         {
+            Console.WriteLine("Number to find the max zero count inbetween 1s in binary digits");
+            Console.WriteLine("Enter the Number: ");
             int num = Convert.ToInt32(Console.ReadLine());
             string BinaryString = Convert.ToString(num, 2);
             Console.WriteLine(BinaryString);
-            //maxConsecutiveZeros(BinaryString);
-            
+            int Maxcount = maxConsecutiveZeros(BinaryString);
+            Console.WriteLine(Maxcount);
+
         }
     }
 }
