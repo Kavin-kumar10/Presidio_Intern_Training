072d02f716b382e1511d1b50e388532b4d6c0ed0
Kavin-kumar10 <kavinkumar.prof@gmail.com>
2024-06-05 17:26:03 +0530

    Profile made responsive

---
+++ Day 33/Profile/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors:{
+        'Primary':'#29303E',
+        'Secondary':'#93969D',
+        'Tertiary':'#249554'
+      }
+    },
+  },
+  plugins: [],
+}
+
