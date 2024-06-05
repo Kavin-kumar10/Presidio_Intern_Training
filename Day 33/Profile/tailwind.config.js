ba53d86c75f8d71639c577003bfb967b2eda7222
Kavin-kumar10 <kavinkumar.prof@gmail.com>
2024-06-05 22:21:36 +0530

    Skills page added

--- Day 33/Profile/tailwind.config.js
+++ Day 33/Profile/tailwind.config.js
@@ -3,6 +3,15 @@ module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
     extend: {
+      keyframes: {
+        beat: {
+          '0%, 100%': { transform:'scale(1,1)' },
+          '50%': { transform: 'scale(0.8,0.8)' },
+        }
+      },
+      animation: {
+        beat: 'beat 5s ease-in-out infinite',
+      },
       colors:{
         'Primary':'#29303E',
         'Secondary':'#93969D',
