61ac589e82e22ff0815132fb367c1036c8718198
Kavin-kumar10 <kavinkumar.prof@gmail.com>
2024-06-11 09:02:36 +0530

    figma assignement and front end for mini project initiated

---
+++ 10 june - have front end for mini project/Refund Management/tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      // colors:{
+      //   primary:'#EDDECC',
+      //   secondary:'#C49575',
+      //   tertiary:'#FFFFFF'
+      // }
+      colors:{
+        primary:'#F0F0F5',
+        secondary:'#1F1F29',
+        tertiary:'#FFFFFF'
+      }
+    },
+  },
+  plugins: [],
+}
+
